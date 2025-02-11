aaed77f7cfe67b4c7040a914316df69e0f20f0cf
godwinyeboah1 <godwin.y15@gmail.com>
2025-02-11 15:06:34 -0500

    Setup react hook form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,18 @@
 import React, { FormEvent, useRef } from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
 
 export const Form = () => {
+
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
   // const user = {name: '', age: 0};
   const [user , setUser] = React.useState({name: '', age: ''});
+
+  const {register, handleSubmit} = useForm();
+  const onSubmit = (data: FieldValues)=> {
+    console.log('submitted', data);
+  }
+
 
   const handleformSubmit = (event:FormEvent) => {
     event.preventDefault();
@@ -15,13 +23,13 @@ export const Form = () => {
     console.log(user);
   }
   return (
-    <form onSubmit={handleformSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3"><label htmlFor="name" className="form-label">Name</label>
-            <input value={user.name} onChange={(e)=> setUser({...user, name: e.target.value })} id="name" type="text" className="form-control" />
+            <input {...register('name')} id="name" type="text" className="form-control" />
         </div>
         <div className="mb-3">
             <label htmlFor="age" className="form-label">Age</label>
-            <input value={user.age} onChange={(e)=> setUser({...user, age: parseInt(e.target.value) })} id="age" type="number" className="form-control" />
+            <input {...register('age')} id="age" type="number" className="form-control" />
           </div>
           <button className="btn btn-primary" type='submit'>Submit</button>
     </form>
